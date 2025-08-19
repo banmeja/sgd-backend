066ff73ff0e22feb74f0c6e7d8a3276f53d7b19f
ban <banmejia@gmail.com>
2025-08-19 15:35:41 -0600

    Configuracion incial

---
+++ src/main/java/gt/sgd/sgdbackend/SgdBackendApplication.java
@@ -0,0 +1,11 @@
+package gt.sgd.sgdbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+public class SgdBackendApplication {
+	   public static void main(String[] args) {
+	        SpringApplication.run(SgdBackendApplication.class, args);
+	    }
+
+}
